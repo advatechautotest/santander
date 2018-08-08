fa9088108fa2bc1ee1a5bd530c78e5c8e03e5a8c
smokehead <artur.wojtowicz1988@gmail.com>
2018-08-08 22:11:00 +0200

    new

--- regresion test/src/test/java/santander/regresion/TestBase.java
+++ regresion test/src/test/java/santander/regresion/TestBase.java
@@ -2,14 +2,16 @@ package santander.regresion;
 
 
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import santander.regresion.menadżerAplikacji.MenadżerAplikacji;
 
 
 public class TestBase {
 
-    protected final MenadżerAplikacji app = new MenadżerAplikacji();
-
+    //protected static final MenadżerAplikacji app = new MenadżerAplikacji();
+    protected static final MenadżerAplikacji app = new MenadżerAplikacji();
     @BeforeMethod
 
     public void setUp() throws Exception {
