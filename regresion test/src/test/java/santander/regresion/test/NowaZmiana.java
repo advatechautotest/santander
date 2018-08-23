075c036bc4461a74e7752d48713715f861acafdf
smokehead <artur.wojtowicz1988@gmail.com>
2018-08-23 10:55:57 +0200

    Revert "Revert "Revert "zmianav1"""
    This reverts commit bb627b16560888a0621c588f1ef9f76909a06df1.

--- regresion test/src/test/java/santander/regresion/test/NowaZmiana.java
+++ regresion test/src/test/java/santander/regresion/test/NowaZmiana.java
@@ -10,11 +10,17 @@ public class NowaZmiana extends TestBase {
 
 
         app.logIn1wszaLinia();
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> parent of bb627b1... Revert "Revert "zmianav1""
         app.screenShot();
         app.getGrupa().Nowy();
         app.screenShot();
         app.getSkrot().ZmianaStandardowa();
         app.screenShot();
+<<<<<<< HEAD
         app.getZmiana().SzablonZmianyDRS_FIX_PILNY_Z3();
         app.screenShot();
         app.saveButton();
@@ -22,5 +28,9 @@ public class NowaZmiana extends TestBase {
     }
 }
 
+=======
+
+>>>>>>> parent of f6833d0... zmianav1
+>>>>>>> parent of bb627b1... Revert "Revert "zmianav1""
     }
 }
